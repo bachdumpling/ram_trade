ec34c048b7820cb1a58621efa833f2b9fc15f5fe
Bach Le <100985625+bachdumpling@users.noreply.github.com>
2024-02-07 07:04:59 -0500

    Only allow Fordham.Edu email

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,7 +5,6 @@ import 'package:ram_trade/screens/profile_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'dart:developer' as developer;
 
-
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -36,7 +35,6 @@ class _LoginScreenState extends State<LoginScreen> {
   Future<AuthResponse> _googleSignIn() async {
     const webClientId =
         '907456851423-9gq0fj471scnt10agb8henf57irf42si.apps.googleusercontent.com';
-
     const iosClientId =
         '907456851423-1gjknjh7cvgu8nesc9092t41gqcu4rmm.apps.googleusercontent.com';
 
@@ -45,10 +43,14 @@ class _LoginScreenState extends State<LoginScreen> {
       serverClientId: webClientId,
     );
     final googleUser = await googleSignIn.signIn();
-    final googleAuth = await googleUser!.authentication;
+    // Ensure the user is not null
+    if (googleUser == null) {
+      throw 'Google Sign In was cancelled by the user.';
+    }
+
+    final googleAuth = await googleUser.authentication;
     final accessToken = googleAuth.accessToken;
     final idToken = googleAuth.idToken;
-
 
     if (accessToken == null) {
       throw 'No Access Token found.';
@@ -57,15 +59,25 @@ class _LoginScreenState extends State<LoginScreen> {
       throw 'No ID Token found.';
     }
 
-    final user = googleSignIn.currentUser;
-    print("user's email: $user");
+    // Check the user's email domain
+    final String? email = googleUser.email;
+    if (email == null || !email.endsWith('@fordham.edu')) {
+      // Optionally show an error message to the user
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text('Only Fordham University emails are allowed.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14, color: Colors.red))),
+      );
+      throw 'Only Fordham University accounts are allowed to sign in.';
+    }
 
+    // If the email domain is correct, proceed with signing in
     return supabase.auth.signInWithIdToken(
       provider: OAuthProvider.google,
       idToken: idToken,
       accessToken: accessToken,
     );
-    
   }
 
   @override
